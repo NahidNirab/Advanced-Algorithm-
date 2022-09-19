03fde69128359dd50713a86e2ac44c76cdda11df
MD NAHIDUL HOQUE NIRAB <nirabnahid@gmail.com>
2022-09-19 22:46:06 +0600

    Add files via upload

--- Practice.cpp
+++ Practice.cpp
@@ -1,171 +1,86 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+#include <string.h>
 using namespace std;
-int getMax(int num1, int num2)
+
+void LPS_table(char pat[], int M, int lps[])
 {
- if(num1<num2)
- {
- return num2;
- }
- else
- {
- return num1;
- }
+
+    int len = 0;
+    lps[0] = 0;
+    int i = 1;
+    while (i < M)
+        {
+            if (pat[i] == pat[len])
+            {
+                len++;
+                lps[i] = len;
+                i++;
+            }
+            else
+            {
+                if (len != 0)
+                {
+                    len = lps[len - 1];
+                }
+                else
+                {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+    cout<<"\nPLS Table = ";
+    for(int i=0;i<M;i++)
+    {
+        cout<<lps[i]<<" ";
+    }
 }
-int Max(int num1,int num2,int num3)
+
+void KMP_patten_Search(char pat[], char txt[],int N,int M)
 {
- if(num1>num2 && num1>num3)
- {
- return num1;
- }
- else if(num2>num1 && num2>num3)
- {
- return num2;
- }
- else if(num3>num2 && num3>num1)
- {
- return num3;
- }
+    int lps[M];
+    LPS_table(pat, M, lps);
+    int i = 0;
+    int j = 0;
+    while (i < N)
+        {
+            if (pat[j] == txt[i])
+            {
+                j++;
+                i++;
+                if(j == M)
+                {
+                    cout<<"\nFound pattern at index = "<<i-j;
+                    j = lps[j - 1];
+                }
+            }
+            else
+            {
+                if (j != 0)
+                {
+                    j = lps[j - 1];
+                }
+                else
+                {
+                    i = i + 1;
+                }
+            }
+        }
 }
-int maximum_value_sell(int c, int r, int matrix[50][50])
-{
- int mat[r][c];
- for(int i=0;i<r;i++)
- {
- for(int j=0;j<c;j++)
- {
- mat[i][j]=matrix[i][j];
- }
- }
- int j=0;
- while(j<c)
- {
- for(int i=0;i<r;i++)
- {
- if(i==0 && j==0)
- {
- continue;
- }
- else if(i!=0 && j==0)
- {
- continue;
- }
- else if(i==0 && j!=0)
- {
- mat[i][j] = mat[i][j] + getMax(mat[i][j-1],mat[i+1][j-1]);
- }
- else if(i==(r-1) && j!=0)
- {
- mat[i][j] = mat[i][j]+getMax(mat[i-1][j-1],mat[i][j-1]);
- }
- else
- {
- mat[i][j]=mat[i][j]+Max(mat[i-1][j-1],mat[i][j-1],mat[i+1][j-1]);
- }
- }
- j++;
- }
- int maximum=0;
- int f=c-1;
- for(int i=0;i<r;i++)
- {
- if(maximum<=mat[i][f])
- {
- maximum = mat[i][f];
- }
- }
- int sell;
- cout<<"sells are = ";
- for(int i=0;i<r;i++)
- {
- if(maximum==mat[i][f])
- {
- sell = i;
- cout<<"("<<i<<","<<f<<"),";
- }
- }
- cout<<endl<<endl;
- return sell;
-}
-int Gold_mine(int c, int r, int matrix[50][50])
-{
- int mat[r][c];
- for(int i=0;i<r;i++)
- {
- for(int j=0;j<c;j++)
- {
- mat[i][j]=matrix[i][j];
- }
- }
- int j=0;
- while(j<c)
- {
- for(int i=0;i<r;i++)
- {
- if(i==0 && j==0)
- {
- continue;
- }
- else if(i!=0 && j==0)
- {
- continue;
- }
- else if(i==0 && j!=0)
- {
- mat[i][j] = mat[i][j] + getMax(mat[i][j-1],mat[i+1][j-1]);
- }
- else if(i==(r-1) && j!=0)
- {
- mat[i][j] = mat[i][j]+getMax(mat[i-1][j-1],mat[i][j-1]);
- }
- else
- {
- mat[i][j]=mat[i][j]+Max(mat[i-1][j-1],mat[i][j-1],mat[i+1][j-1]);
- }
- }
- j++;
- }
- cout<<"Gold mine table : "<<endl;
- for(int i=0;i<r;i++)
- {
- for(int j=0;j<c;j++)
- {
- cout<<mat[i][j]<<" ";
- }
- cout<<endl;
- }
- cout<<endl;
- int maximum=0;
- int f=c-1;
- for(int i=0;i<r;i++)
- {
- if(maximum<=mat[i][f])
- {
- maximum = mat[i][f];
- }
- }
- return maximum;
-}
+
 int main()
 {
- int row,col;
- cout<<"Enter Row and Column of the matrix\n";
- cout<<"enter Row= ";
- cin>>row;
- cout<<"enter column= ";
- cin>>col;
- int matrix[50][50];
- cout<<"enter matrix: "<<endl;
- for(int i=0;i<row;i++)
- {
- for(int j=0;j<col;j++)
- {
- cout<<"inputArray["<<i<<"]["<<j<<"] = ";
- cin>>matrix[i][j];
- }
- cout<<endl;
- }
- cout<<"maximum gold found is = "<<Gold_mine(col, row, matrix)<<endl;
- cout<<"one of the sell that we return in main faction is = ("<<maximum_value_sell(col,
-row, matrix)<<","<<row-1<<")"<<endl;
- return 0;
+
+    char text[100];
+    char patten[100];
+    cout<<"Input your text array = ";
+    cin.get (text,100);
+    cout<<"\nInput your patten array = ";
+    cin>>patten;
+    int t_size = strlen(text);
+    int p_size = strlen(patten);
+    cout<<"\nThe size of your text array is = "<<t_size;
+    cout<<"\nThe size of your patten array is = "<<p_size;
+    KMP_patten_Search(patten, text, t_size, p_size);
+    return 0;
 }
